06d9b841649016187ae924550222c052d0c833ea
sz16900 <sz16900@my.bristol.ac.uk>
2020-04-17 11:24:43 -0500

    :ambulance: extra credit
    add error messages and handling

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,17 +7,16 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def show
-  end
+  def show; end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-    redirect_to new_user_path
+      redirect_to new_user_path
     else
-    render :edit
+      render :edit
     end
-  end 
+  end
 
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
@@ -37,7 +36,6 @@ class UsersController < ApplicationController
   end
 
   def user_params
-      params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :password)
   end
-
 end
