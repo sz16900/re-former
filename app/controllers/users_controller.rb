f563a29dfae29ec77e838ef3c199a7a7a833b6ae
Luis Saavedra <nriqu322@gmail.com>
2020-04-17 10:03:23 -0600

    added edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,22 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+    redirect_to new_user_path
+    else
+    render :edit
+    end
+  end 
+
   def create
     # @user = User.new(username: params[:username], email: params[:email], password: params[:password])
     @user = User.new(user_params)
@@ -15,6 +31,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
       params.require(:user).permit(:username, :email, :password)
